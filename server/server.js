1a0646dd56a32e46241a251f7f9f63c0c8cd95d1
Imtiaz4530 <apual4016@gmail.com>
2025-10-31 12:46:51 +0600

    10th commit - PC - correcting vercel.json - still some work pending

--- server/server.js
+++ server/server.js
@@ -1,59 +1,33 @@
-// require('dotenv').config();
-// const express = require('express');
-// const cors = require('cors');
-
-// const connectDB = require('./db/connectDB');
-// const userRoutes = require('./routes/user.routes');
-// const storyRoutes = require('./routes/story.routes');
-
-// const app = express();
-
-// // Connect to the database
-// app.use(express.json());
-// app.use(cors({
-//   origin: process.env.CLIENT_URL, 
-//   credentials: true,
-// }));
-// connectDB();
-
-
-// app.get('/', (req, res) => {
-//     res.send('Hello from the server!');
-// });
-
-// app.use("/api/user", userRoutes)
-// app.use("/api/story", storyRoutes)
-
-// const PORT = process.env.PORT || 4000;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
 const connectDB = require('./db/connectDB');
 const userRoutes = require('./routes/user.routes');
 const storyRoutes = require('./routes/story.routes');
 
 const app = express();
 
-// Middleware
+// Connect to the database
 app.use(express.json());
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: process.env.CLIENT_URL, 
   credentials: true,
 }));
-
-// Connect to database
 connectDB();
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('✅ API is working!');
-});
-app.use('/api/user', userRoutes);
-app.use('/api/story', storyRoutes);
 
-module.exports = app;
+app.get('/', (req, res) => {
+    res.send('Hello from the server!');
+});
+
+app.use("/api/user", userRoutes)
+app.use("/api/story", storyRoutes)
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
